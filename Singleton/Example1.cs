44b831030c2945a89d13af96418484fe732fba05
Admin <Admin@ASU-FLEX>
2020-12-07 21:33:17 +0300

    home

---
+++ Singleton/Example1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    class OS
+    {
+        private static OS instance;
+        public string NameOS { get; private set; }
+        private OS(string n) {
+            NameOS = n;
+        }
+        public static OS GetInstance(string nameOs)
+        {
+            if (instance == null)
+                instance = new OS(nameOs);
+            return instance;
+        }
+    }
+    class Computer
+    {
+        public OS OS { get; set; }
+        public void Launch(string nameOS)
+        {
+            OS = OS.GetInstance(nameOS);
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread myThread = new Thread(() =>
+            {
+                Thread.Sleep(190);
+                Computer comp2 = new Computer();
+                comp2.Launch("Windows10");
+                Console.WriteLine("MyComp2" + comp2.OS.NameOS);
+            });
+            myThread.Start();
+            Thread.Sleep(200);
+            Computer computer = new Computer();
+            computer.Launch("Windows7");
+            Console.WriteLine(computer.OS.NameOS);
+            computer.Launch("Windows10");
+            computer.OS = OS.GetInstance("WindowsServer2003"); 
+            Console.WriteLine(computer.OS.NameOS);
+            Console.ReadKey();
+        }
+    }
+}
