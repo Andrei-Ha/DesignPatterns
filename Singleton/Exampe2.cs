fad6995dc3857ec8d4f260ac41a29ad6aa8b2061
Андрей Сергеич <asgoreglyad@gmail.com>
2020-12-07 16:58:02 +0300

    singleton from work

---
+++ Singleton/Exampe2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    class OS
+    {
+        private static object syncRoot = new object();
+        private static OS _instance;
+        public string Name { get; private set; }
+        private OS(string name)
+        {
+            Name = name;
+        }
+        public static OS GetInstance(string n)
+        {
+            lock (syncRoot)
+            {
+                if (_instance == null)
+                    _instance = new OS(n);
+            }
+            return _instance;
+        }
+    }
+    class Comp
+    {
+        public OS Os { get; set; }
+        public void Launch(string nameOs)
+        {
+            Os = OS.GetInstance(nameOs);
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (new Thread(() =>
+            {
+                Comp newComp = new Comp();
+                newComp.Os = OS.GetInstance("Windows8");
+                Console.WriteLine($"in new thread running \"{newComp.Os.Name}\"");
+            })).Start();
+            Thread.Sleep(1);
+            Comp myComp = new Comp();
+            myComp.Launch("Windows10");
+            Console.WriteLine(myComp.Os.Name);
+            myComp.Launch("Windows7");
+            Console.WriteLine(myComp.Os.Name);
+            Console.ReadKey();
+        }
+    }
+}
