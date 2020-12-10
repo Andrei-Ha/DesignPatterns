58497d5538f7a759b1919875bb199cdf47baa800
Андрей Сергеич <asgoreglyad@gmail.com>
2020-12-10 16:52:42 +0300

    composite in work

--- Composite/Example.cs
+++ Composite/Example.cs
@@ -4,12 +4,24 @@ using System.Text;
 
 namespace Composite
 {
+    static class Andr
+    {
+        public static string Tab(int q)
+        {
+            string result = "";
+            for (int i = 0; i < q; i++)
+                result += "     ";
+            return result;
+        }
+    }
     abstract class Component
     {
         private string _name;
+        private int _size=0;
         public string Name { get => _name; set => _name = value; }
+        public int Size { get => _size; set => _size = value; }
         public Component(string name) => Name = name;
-        public virtual void Print() => Console.WriteLine(Name);
+        public virtual void Print(int i=0) => Console.WriteLine(Andr.Tab(i) + Name);
         public virtual void Add(Component component) { }
         public virtual void Remove(Component component) { }
     }
@@ -23,14 +35,17 @@ namespace Composite
         public Folder(string name) : base(name) { }
         public override void Add(Component component) => children.Add(component);
         public override void Remove(Component component) => children.Remove(component);
-        public override void Print()
+        public override void Print(int i)
         {
             //string result = $"Название папки:{Name}. Содержимое: ";
-            Console.WriteLine("Node:" + Name);
-            Console.WriteLine("subnode:");
+            //Console.WriteLine(i.ToString() + Name);
+            //Console.WriteLine("subnode:");
+            Console.ForegroundColor = ConsoleColor.Red;
+            base.Print(i);
+            Console.ForegroundColor = ConsoleColor.White;
             foreach(Component child in children)
             {
-                child.Print();
+                child.Print(i+1);
             }
         }
     }
--- Composite/Program.cs
+++ Composite/Program.cs
@@ -9,12 +9,18 @@ namespace Composite
             Component jpg = new File("pics.jpg");
             Component pdf = new File("readme.pdf");
             Component pictures = new Folder("MyPictures");
-            Component c = new Folder("C");
-            Component boot = new File("boot.ini");
             pictures.Add(jpg);
             pictures.Add(pdf);
+            Component temp_folder = new Folder("Temp");
+            temp_folder.Add(new File("readmi.txt"));
+            pictures.Add(temp_folder);
+
+            Component c = new Folder("C");
+            Component boot = new File("boot.ini");
+            
             c.Add(boot);
             c.Add(pictures);
+            c.Add(temp_folder);
             c.Print();
             Console.ReadKey();
         }
