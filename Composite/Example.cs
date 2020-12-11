6237e5bbdd8bb13d8d7e2bc6ac10c3ea56cfdee1
Андрей Сергеич <asgoreglyad@gmail.com>
2020-12-11 17:00:57 +0300

    finally in work

--- Composite/Example.cs
+++ Composite/Example.cs
@@ -17,29 +17,44 @@ namespace Composite
     abstract class Component
     {
         private string _name;
-        private int _size=0;
+        private int _size = 0;
         public string Name { get => _name; set => _name = value; }
         public int Size { get => _size; set => _size = value; }
-        public Component(string name) => Name = name;
+        public Component(string name, int size)
+        {
+            Name = name;
+            Size = size;
+        }
         public virtual void Print(int i=0) => Console.WriteLine(Andr.Tab(i) + $"{Name}({Size})");
         public virtual void Add(Component component) { }
         public virtual void Remove(Component component) { }
+        public virtual void RecalculateSize() { }
+        public virtual bool IsFolder() => false;
     }
     class File : Component
     {
-        public File(string name) : base(name) { }
+        public File(string name, int size) : base(name,size) { }
+
     }
     class Folder: Component
     {
         private List<Component> children = new List<Component>();
-        public Folder(string name) : base(name) { }
+        public Folder(string name) : base(name, 0) { }
         public override void Add(Component component) => children.Add(component);
         public override void Remove(Component component) => children.Remove(component);
+        public override bool IsFolder() => true;
+        public override void RecalculateSize()
+        {
+                Size = 0;
+            foreach (Component child in children)
+            {
+                if (child.IsFolder())
+                    child.RecalculateSize();
+                Size += child.Size;
+            }
+        }
         public override void Print(int i)
         {
-            //string result = $"Название папки:{Name}. Содержимое: ";
-            //Console.WriteLine(i.ToString() + Name);
-            //Console.WriteLine("subnode:");
             Console.ForegroundColor = ConsoleColor.Red;
             base.Print(i);
             Console.ForegroundColor = ConsoleColor.White;
--- Composite/Program.cs
+++ Composite/Program.cs
@@ -6,21 +6,27 @@ namespace Composite
     {
         static void Main(string[] args)
         {
-            Component jpg = new File("pics.jpg");
-            Component pdf = new File("readme.pdf");
+            Component jpg = new File("pics.jpg",3);
+            Component pdf = new File("readme.pdf",7);
             Component pictures = new Folder("MyPictures");
             pictures.Add(jpg);
             pictures.Add(pdf);
             Component temp_folder = new Folder("Temp");
-            temp_folder.Add(new File("readmi.txt"));
+            temp_folder.Add(new File("readme.txt",23));
             pictures.Add(temp_folder);
 
             Component c = new Folder("C");
-            Component boot = new File("boot.ini");
-            
+            Component boot = new File("boot.ini",45);
+            new Folder("");
             c.Add(boot);
             c.Add(pictures);
-            c.Add(temp_folder);
+            c.Add(new File("cmd.exe",9));
+            Component win_folder = new Folder("windows");
+            win_folder.Add(new File("one.doc", 5));
+            win_folder.Add(new Folder("system"));
+            win_folder.Add(new File("two.docx", 3));
+            c.Add(win_folder);
+            c.RecalculateSize();
             c.Print();
             Console.ReadKey();
         }
