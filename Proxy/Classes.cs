a275d57959a3161560f7ee0440fc84071d34a320
Андрей Сергеич <asgoreglyad@gmail.com>
2020-12-18 16:15:17 +0300

    proxy in work

---
+++ Proxy/Classes.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proxy
+{
+    static class Book
+    {
+        private static List<string> listReal = new List<string>(new string[]{"spring", "summer", "autumn", "winter"});
+        public static string GetPage(int i) => listReal[i];
+    }
+    abstract class Subject
+    {
+        abstract public string Request(int i);
+    }
+    class RealSubject: Subject
+    {
+        public override string Request(int i)
+        {
+            return ($"page number {i}:" +Book.GetPage(i));
+        }
+    }
+    class ProxySubject: Subject
+    {
+        RealSubject realSubject;
+        Dictionary<int, string> dict;
+        public ProxySubject()
+        {
+            if (realSubject == null)
+                realSubject = new RealSubject();
+        }
+        public override string Request(int i)
+        {
+            if (dict == null)
+                dict = new Dictionary<int, string>();
+            string dictResult;
+            if (!dict.TryGetValue(i, out dictResult))
+            {
+                string result = realSubject.Request(i);
+                dict.Add(i, result);
+                return result;
+            }
+            else
+                return dictResult + " from cache";
+        }
+    }
+}
---
+++ Proxy/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Subject realSubject = new RealSubject();
+            Console.WriteLine(realSubject.Request(3));
+            Console.WriteLine( );
+            Subject wiaProxy = new ProxySubject();
+            Console.WriteLine(wiaProxy.Request(1));
+            Console.WriteLine(wiaProxy.Request(2));
+            Console.WriteLine(wiaProxy.Request(1));
+            Console.WriteLine(wiaProxy.Request(0));
+            Console.WriteLine(wiaProxy.Request(3));
+            Console.WriteLine(wiaProxy.Request(2));
+            Console.WriteLine(wiaProxy.Request(0));
+            Console.WriteLine(wiaProxy.Request(3));
+            Console.WriteLine(wiaProxy.Request(1));
+        }
+    }
+}
