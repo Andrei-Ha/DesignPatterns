0828501c05f542e6273dacbafd833c92e1956383
Андрей Сергеич <asgoreglyad@gmail.com>
2021-01-06 16:05:48 +0300

    work before weekend

---
+++ Iterator/Classes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Iterator
+{
+    class Numbers
+    {
+        private int[] _nums;
+        public Numbers(int[] nums)
+        {
+            _nums = nums;
+        }
+        public IEnumerator<int> GetEnumerator()
+        {
+            foreach (int i in _nums)
+                yield return i + 2;
+        }
+    }
+}
---
+++ Iterator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+
+namespace Iterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable actors = new ArrayList(); //new string[] { "first", "second", "third" };
+            IEnumerator perechislitel = actors.GetEnumerator();
+
+            Numbers numbers = new Numbers(new int[] { 2, 4, 5, 7, 9, 11 });
+            foreach(int i in numbers)
+                Console.WriteLine(i);
+            Console.ReadKey();
+        }
+    }
+}
